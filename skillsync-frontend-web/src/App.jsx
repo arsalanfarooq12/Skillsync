7fe1678a0b12a00c054213757928677fd3a23824
arsalanfarooq12 <145360951+arsalanfarooq12@users.noreply.github.com>
2026-04-16 15:53:29 +0530

    feat: add routing and authentication pages for login, registration, and trades

--- skillsync-frontend-web/src/App.jsx
+++ skillsync-frontend-web/src/App.jsx
@@ -1,5 +1,31 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+
+import TradesPage from "./pages/TradesPage";
+
 function App() {
-  return <h1>SkillSync</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/trades" replace />} />
+
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+
+        <Route
+          path="/trades"
+          element={
+            <ProtectedRoute>
+              <TradesPage />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ skillsync-frontend-web/src/pages/TradesPage.jsx
@@ -0,0 +1,4 @@
+function TradesPage() {
+  return <h1>Trades</h1>;
+}
+export default TradesPage;
