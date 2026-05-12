7c4157658ffc37a635113a66dd6e7306bcbd14c4
arsalanfarooq12 <145360951+arsalanfarooq12@users.noreply.github.com>
2026-05-12 18:53:24 +0530

    adding netlify origin

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(helmet());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://skillsy.netlify.app/"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
   })
